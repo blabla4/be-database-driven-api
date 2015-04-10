a3f2ed690ebb4b447af880686f56296a3c27fedd
blabla4 <amaury.cannesson@gmail.com>
2015-04-10 13:02:02 +0200

    update model ref

--- models/api.js
+++ models/api.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var apiSchema = mongoose.Schema({
   host: String,
   basePath: String,
-  methods: Array
+  methods: [{ type: mongoose.Schema.Types.ObjectId, ref: 'method' }]
 }, {collection: 'api'});
 
 module.exports = mongoose.model('api', apiSchema);
