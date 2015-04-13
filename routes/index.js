db0d3e72cf6d211fa93b0ca160a9bb68718a8747
blabla4 <amaury.cannesson@gmail.com>
2015-04-13 12:03:55 +0200

    design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var restClient = require('restler');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  restClient.get('http://localhost:3000/apis?populate=methods').on('complete', function(data) {
+  restClient.get('http://10.134.15.103/apis?populate=methods').on('complete', function(data) {
     res.render('index', {apis: data});
   });
 });
