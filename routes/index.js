66c9df014ebfcd2e10ea33d722c58fd84da3bd2a
blabla4 <amaury.cannesson@gmail.com>
2015-04-14 12:42:34 +0200

    shortcut url

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,16 @@ router.get('/', function(req, res) {
   });
 });
 
+router.get('/3d', function(req, res) {
+  res.redirect('http://10.134.15.103:3002/');
+});
+
+router.get('/tv', function(req, res) {
+  res.redirect('http://10.134.15.103:3000/');
+});
+
+router.get('/table', function(req, res) {
+  res.redirect('http://10.134.15.103:3001/');
+});
+
 module.exports = router;
